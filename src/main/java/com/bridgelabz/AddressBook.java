eaad6194022123770f3df9b88ff3b0a375ca282d
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-05-06 22:11:14 +0530

    Added : Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...

---
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        Person person = new Person("Kamat", "Vinayak", "Mumbai",
+                "Maharashtra", 400051, "Bandra", 9874545630L,
+                "vinayak@akamat");
+        System.out.println(person);
+    }
+
+}
--- src/main/java/com/bridgelabz/Person.java
+++ src/main/java/com/bridgelabz/Person.java
@@ -1,7 +1,98 @@
 package com.bridgelabz;
 
 public class Person {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+    private String lastname;// Stores lastname of Person
+    private String firstname;// Stores firstname of Person
+    private String address;// Stores address of Person
+    private String city;// Stores city of Person
+    private String state;// Stores state of Person
+    private int zip;// Stores zip of Person
+    private long number;// Stores number of Person
+    private String email;// Stores email of Person
+
+    public Person(String lastname, String firstname, String city,
+                  String state, int zip, String address, long number, String email)
+    {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.address = address;
+        this.number = number;
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + firstname + " " + lastname + "\n" +
+                "address: " + address + "\n" + "City: " + city +
+                "\n" +"State: " + state + "\n" + "Zip: " + zip +
+                "\n" + "Number# " + number + "\n" + "Email: " + email;
     }
 }
